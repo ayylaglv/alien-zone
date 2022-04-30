a06df5034155181537d45bc02242306dcc695fc1
alphasr <nilaybaranwal@gmail.com>
2022-04-30 21:48:09 +0200

    inital commit

---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <div className="footer-text">Contact FAQs</div>
+    </footer>
+  );
+}
+
+export default Footer;
